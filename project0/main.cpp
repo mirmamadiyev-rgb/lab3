a1cfd7d1f91b93c7673404a35b3bb5cb12c45d01
mirmamadiyev-rgb <mir.mamadiyev@newuu.uz>
2025-09-11 11:05:52 +0500

    Add files via upload

---
+++ project0/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+//problem1
+/* int main() {
+    int intNumber=17;
+    float floatNumber=3.14159;
+    double doubleNumber=45.1234;
+    bool boolean=true;
+    char charName = 'A';
+    cout<<intNumber<<sizeof(intNumber)<< endl;
+    cout<<intNumber<<"size :" << sizeof(floatNumber)<<endl;
+    cout<<floatNumber<<"size :" << sizeof(doubleNumber)<<endl;
+    cout<<boolean<<"size :" << sizeof(boolean)<<endl;
+    cout<<charName<<"size :" << sizeof(charName)<<endl;
+}
+*/
+
+
+
+
+//problem2
+/*int main() {
+    string name= "Mirzohid";
+    int ID= 240340;
+    string favourite_song= "sultane qalbam";
+    string favourite_movie = "Maverick";
+    cout << "Name :" << name << endl;
+     cout << "ID: " << ID << endl;
+    cout << "favourite song : " << favourite_song << endl;
+    cout << "favourite movie : " << favourite_movie << endl;
+
+    return 0;
+
+} */
+//problem3
+int main() {
+    std::cout << R"( ”I’m standing” on the edge of some crazy cliff. What I have to do, I have to catch everybody/if they start to go over the cliff—I mean\
+     \if they’re running and they don’t look where they’re going I have to come out from somewhere and catch them.)" ;
+    return 0;
+}
+
+
