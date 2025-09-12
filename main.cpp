89e12ef5a4a7adc4c00cc6e0edc4920e18606232
mirmamadiyev-rgb <mir.mamadiyev@newuu.uz>
2025-09-12 11:47:42 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+using namespace std;
+//problem10
+/*int main() {
+    double num=1.13e3;
+    double num2=411e-4;
+    cout << num << endl;
+    cout << num2 << endl;
+    return 0;
+} */
+
+
+//problem12
+/*int main() {
+int totalStudents=241;
+    int groups=11;
+    int studentsPerGroup=totalStudents/groups;
+    int remainder=totalStudents%groups;
+    cout<<"Students in the first group: " <<studentsPerGroup<<endl;
+    cout<<"Students in the last group: "<<remainder<<endl;
+    return 0;
+}  */
+
+//problem13
+ /*#include <cmath>
+int main() {
+    double side,area;
+    cout<<"Enter the side of the hexagon: ";
+    cin>>side;
+    area = (3*sqrt(3)/2)*side*side;
+    cout<<"Area of the hexagon is: "<<area<<endl;
+    return 0;
+} */
+//problem14
+  /*int main() {
+int num;
+    cout << "Enter an integer number: ";
+    cin >> num;
+num = num +1;
+    num = num *3;
+    num = num-14;
+    num = num/4;
+    num = num-1;
+    num = num%9;
+    cout<<"Final value of num : " << num << endl;
+    return 0;
+}   */
+
+
+//problem15
+ /*int main() {
+char ch='A';
+    ch+=7;
+    cout<<ch;
+    ch-=3;
+    cout<<ch;
+    ch+=7;
+    cout<<ch;
+    cout<<ch;
+    ch+=3;
+    cout<<ch;
+    ch-=46;
+    cout<<ch;
+    cout<<endl;
+    return 0;
+} */
+
+//problem16
+
+ /*int main() {
+    float x=17;
+    float y=1;
+    float z;
+    z=(x+x+1)/((y+1)*(y+2));
+    cout<<z<<endl;
+    return 0;
+}   */
+
+//problem17
+/* int main() {
+double a;
+    double b;
+    cin>>a;
+    cin>>b;
+    cout<<(a==b)<<endl;
+    return 0;
+} */
+
+//problem18
+/*int main() {
+    double a;
+    double b;
+    cout << "Please enter 2 numbers: ";
+    cin>>a;
+    cin>>b;
+    cout<<(a<140 && a<b)<<endl;
+    return 0;
+} */
+
+//problem19
+ /*int main() {
+    double a;
+    double b;
+    double c;
+cout << "Please enter 3 numbers: ";
+    cin>>a;
+    cin>>b;
+    cin>>c;
+cout<<((a>b)&&(a>c))<<endl;
+} */
+
+//problem20
+/*int main() {
+int a,b,c,d,e;
+    cout<<"Enter five numbers:";
+    cin>>a>>b>>c>>d>>e;
+ cout<<((a>0)||(b>0)||(c>0)||(d>0)||(e>0))<<endl;
+}*/
